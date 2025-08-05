49ceeb41d17d9732667d57301347c712bfd58148
Rachel Quaye-Asamoah <145209780+kenkey8201@users.noreply.github.com>
2025-08-05 17:12:31 -0400

    Create orderbook.h
    The headerfile that defines the main orderbook functions

---
+++ src/orderbook.h
@@ -0,0 +1,16 @@
+#ifndef ORDERBOOK_H
+#define ORDERBOOK_H
+
+#include "utils.h"
+
+//Main order book functions
+void initialize_price_levels(PriceLevel* levels, int max_levels);
+void add_to_price_level(PriceLevel* level, Order* order);
+void remove_from_price_level(PriceLevel* level, const char* order_id);
+void sort_price_levels(PriceLevel* levels, int count, OrderSide side);
+int find_price_level_index(PriceLevel* levels, int count, double price);
+void execute_trade(OrderBook* book, Order* buy_order, Order* sell_order, int quantity);
+void update_order_status(Order* order);
+void cleanup_filled_orders(OrderBook* book);
+
+#endif //ORDERBOOK_H
